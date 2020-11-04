d3ae5a72721bf362554b4a9043ca6e003ea5230d
Konrad <gornikonrad@gmail.com>
2020-11-04 17:26:26 +0100

    add scss mixin,finished mobile menu

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@
 $("document").ready(() => {
     $(".menu-toggle").click(() => {
         $("nav").toggleClass("hide")
+        $(".menu-toggle i").toggleClass("fa-times")
     });
  $("header nav ul li a").click(()=>{
      $("nav").toggleClass("hide")
+     $(".menu-toggle i").toggleClass("fa-times")
  })
 })
