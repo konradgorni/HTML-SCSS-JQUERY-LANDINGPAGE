8eb6f93b982f61ea1c9fb678cd0194f4d5581e9a
Konrad <gornikonrad@gmail.com>
2020-11-05 23:35:40 +0100

    our services section and recent works,carusel

---
+++ js/imagesList.js
@@ -0,0 +1,44 @@
+export const List = [
+
+    {
+        path: "./assets/images/works_example_1.png",
+        category: "ANIMATION"
+    },
+        {
+        path: "./assets/images/works_example_2.png",
+        category: "ANIMATION"
+    },
+        {
+        path: "./assets/images/works_example_3.png",
+        category: "ART"
+    },
+    {
+        path: "./assets/images/works_example_4.png",
+        category: "ART"
+    },
+    {
+        path: "./assets/images/works_example_5.png",
+        category: "WEB DESIGN"
+    },
+    {
+        path: "./assets/images/works_example_6.png",
+        category: "WEB DESIGN"
+    },
+        {
+        path: "./assets/images/works_example_7.png",
+        category: "PHOTOGRAPHY"
+    },
+            {
+        path: "./assets/images/works_example_4.png",
+        category: "PHOTOGRAPHY"
+    },
+                {
+        path: "./assets/images/works_example_2.png",
+        category: "PRINT DESIGN"
+    },
+                {
+        path: "./assets/images/works_example_3.png",
+        category: "PRINT DESIGN"
+    },
+]
+
--- js/index.js
+++ js/index.js
@@ -1,12 +1,110 @@
-/// mobile menu
+import {
+    List
+} from './imagesList.js';
+
 
 $("document").ready(() => {
+
+    //mobile menu
+
+
     $(".menu-toggle").click(() => {
         $("nav").toggleClass("hide")
         $(".menu-toggle i").toggleClass("fa-times")
     });
- $("header nav ul li a").click(()=>{
-     $("nav").toggleClass("hide")
-     $(".menu-toggle i").toggleClass("fa-times")
- })
+    $("header nav ul li a").click(() => {
+        $("nav").toggleClass("hide")
+        $(".menu-toggle i").toggleClass("fa-times")
+    })
+    //carusel
+    let category = "ALL";
+    let currentImage = 0;
+    let categoryArray
+
+    const carusel = (direction, category, categoryArray) => {
+        const img = $(".works_wrapper_img")[0];
+        console.log(`direction ${direction} category ${category} length ${length} 
+        category length${categoryArray.length}`)
+
+        if (direction === "right") {
+            if (category === "ALL") {
+                ++currentImage;
+
+                if (currentImage === List.length - 1) {
+                    currentImage = 0;
+                    img.setAttribute("src", `${List[currentImage].path}`)
+                } else {
+                    //  currentImage++;
+                    img.setAttribute("src", `${List[currentImage].path}`)
+                }
+
+            } else {
+                if (currentImage === categoryArray.length - 1) {
+                    currentImage = 0;
+                    img.setAttribute("src", `${categoryArray[currentImage].path}`)
+                } else {
+                    //  currentImage++;
+                    img.setAttribute("src", `${categoryArray[currentImage].path}`)
+                }
+
+
+
+            }
+
+        }
+        if (direction === "left") {
+            if (category === "ALL") {
+
+                if (currentImage === -1) {
+                    currentImage = List.length - 1;
+                    img.setAttribute("src", `${List[currentImage].path}`)
+                } else {
+
+                    img.setAttribute("src", `${List[currentImage].path}`)
+
+                }
+
+            } else {
+                if (currentImage === -1) {
+                    currentImage = categoryArray.length - 1
+                    img.setAttribute("src", `${categoryArray[currentImage].path}`)
+                } else {
+
+                    img.setAttribute("src", `${categoryArray[currentImage].path}`)
+                }
+
+
+            }
+
+            --currentImage;
+        }
+
+
+
+
+
+
+    };
+//change active category
+    $(".works_category_element").click(function () {
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+        category = this.textContent
+        currentImage = 0;
+
+    })
+
+
+
+
+
+    $(".works_wrapper_button").click((event) => {
+        const direction = event.target.getAttribute('data-target');
+        categoryArray = List.filter(photo => photo.category === category)
+        const categoryArrayLength = categoryArray.length;
+        carusel(direction, category, categoryArray);
+        
+
+
+    })
 })
