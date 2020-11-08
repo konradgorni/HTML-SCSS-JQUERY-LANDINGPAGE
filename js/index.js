6caa4eeb874a6bd657030fc82f077a729ae30ad7
Konrad <gornikonrad@gmail.com>
2020-11-08 15:06:55 +0100

    add 4 new section.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 import {
     List
 } from './imagesList.js';
+import {
+    postsList
+} from './postsList.js'
 
 
 $("document").ready(() => {
@@ -68,11 +71,85 @@ $("document").ready(() => {
             categoryArray = List.filter(photo => photo.category === category)
         }
 
-        const categoryArrayLength = categoryArray.length -1;
+        const categoryArrayLength = categoryArray.length - 1;
         carusel(direction, categoryArray, categoryArrayLength);
-        console.log(categoryArrayLength,categoryArray)
+
 
 
 
     })
+
+
+    //study cart event
+    // $(".works_category_element").click(function () {
+    //     $(".active").removeClass("active");
+    //     $(this).addClass("active");
+    //     category = this.textContent
+    //     currentImage = 0;
+
+    // })
+
+    let currentCard = 0;
+    const changeDot = (direction) => {
+        const dots = $(".study_wrapper_post_dotWrapper_dot");
+        dots[currentCard].classList.remove("active_dot")
+
+
+
+
+        if (direction === "right") {
+            if (currentCard === 2) {
+                dots[0].classList.add("active_dot")
+                currentCard = 0;
+            } else {
+                currentCard++;
+                dots[currentCard].classList.add("active_dot")
+            }
+
+        }
+        if (direction === "left") {
+            if (currentCard === 0) {
+                dots[2].classList.add("active_dot")
+                currentCard = 2;
+            } else {
+                currentCard--;
+                dots[currentCard].classList.add("active_dot")
+
+            }
+
+        }
+
+
+    };
+    const changePosts = () => {
+        let postIcon = $(".study_wrapper_post_square i")[0];
+        let postTitle = $(".study_wrapper_post_title")[0];
+        let postText = $(".study_wrapper_post_text")[0];
+        
+        
+        postIcon.classList=postsList[currentCard].iconClass;
+
+
+
+        postTitle.textContent=postsList[currentCard].title;
+        postText.textContent=postsList[currentCard].description;
+
+    };
+
+    ///cash post
+changePosts()
+
+    $(".study_wrapper_post").on("swipeleft", function () {
+      
+
+        $(".active_dot").removeClass("active_dot");
+        changeDot("right");
+        changePosts();
+    });
+    $(".study_wrapper_post").on("swiperight", function () {
+        changeDot("left");
+        changePosts();
+    });
+    // 
+
 })
